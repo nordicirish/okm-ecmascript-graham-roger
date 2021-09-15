219130b38b4ba50fcbf8cf2d9bc46071d07326b7
Roger Graham <rogergraham44@gmail.com>
2021-09-15 20:51:55 +0300

    Update e06.js
    modified testLet function and added trycatch to testLet and testConst

--- homework/2020-35/e03-e13/e06.js
+++ homework/2020-35/e03-e13/e06.js
@@ -1,26 +1,36 @@
 var a = 10; // globally scoped var a
 function testVar() {
-  //functionally scoped var a is hoisted to top
+  //functionally scoped var a is hoisted to top - undefiined value as value remains below
   console.log("a is " + a); // output undefined - value is not hoisted from below with the variable
   var a = 20; // functionally scoped var a - var allows two different variables to have the same name
   console.log("a is " + a); // output 20
 }
 testVar();
-console.log("a is " + a); //output 10 - global var value
+console.log("a is " + a); //output 10 - globally scoped var value
 
+// let is block scoped
 function testLet() {
-  let b = 10;
-  console.log("b is " + b); // b is 10
-  b = 20; // unable to use let to declare 2 variables of the same name
-  console.log("b is " + b); // output 20
+  for (let i = 0; i < 3; i++) {
+    console.log(i);
+  }
 }
+
 testLet();
-// console.log("b is " + b); --- --- Uncaught ReferenceError: b is not defined as b is block scoped and not available globally
+// can't be referenced oustside the block
+try {
+  console.log(i);
+} catch (error) {
+  console.error(error);
+}
 
 function testConst() {
   const c = 10;
   console.log("c is " + c); //c is 10
   c = 20;
-  console.log("c is " + c); //Uncaught TypeError: Assignment to constant variable -- const values cannot be changed.
+  try {
+    console.log("c is " + c); //Uncaught TypeError: Assignment to constant variable -- const values cannot be changed.
+  } catch (error) {
+    console.error(error);
+  }
 }
 testConst();
