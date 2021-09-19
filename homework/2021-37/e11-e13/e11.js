6e25bad4289715db529132463dd5c811841d1a54
Roger Graham <rogergraham44@gmail.com>
2021-09-19 19:01:19 +0300

    Update e11.js
    Update to accept node command line args
    eg node e11.js 1, node e11.js 2 etc and return the star wars character matching the id entered

--- homework/2021-37/e11-e13/e11.js
+++ homework/2021-37/e11-e13/e11.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
+const args = process.argv.slice(2); // slice (2) removes the first 2 arguments from the node command line
+// https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line
 const part1 = () =>
-  axios.get("https://swapi.dev/api/people/1").then((response) => {
+  axios.get("https://swapi.dev/api/people/" + args).then((response) => {
     let starWars = response.data;
-    console.log(starWars.name); //returns data for Luke Skywalker
+    console.log(starWars.name); //returns data for Luke Skywalker if node e11.js 1 is used
   });
 part1();
