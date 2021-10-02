682c260f770522d9af4ae68f4ffa75f92f7e82d6
Roger Graham <rogergraham44@gmail.com>
2021-10-02 23:50:55 +0300

    exercise 4c implemented

--- homework/2021-39/e03/app.js
+++ homework/2021-39/e03/app.js
@@ -5,21 +5,30 @@ function main() {
   //   console.log("The file name is " + fileName);
   readFile(fileName);
 }
+
 function readFile(fileName) {
   const fs = require("fs");
   fs.readFile(fileName, "utf-8", (err, data) => {
     //   callback function receives err and data args
-    try {
-      const obj = JSON.parse("this is not json");
-    } catch (err) {
-      console.log(`file ${fileName} does not contain json`);
+    if (err) {
+      console.log("Filename does not exist");
+    } else {
+      try {
+        const obj = JSON.parse(data);
+        // console.log("valid json");
+        //   valid json test
+        try {
+          if ("name" in obj) {
+            console.log(`name is ${obj.name}`);
+          }
+          // name key test
+        } catch (err) {
+          console.log(`file ${fileName} contains json but not property name`);
+        }
+      } catch (err) {
+        console.log(`file ${fileName} does not contain json`);
+      }
     }
-
-    // if (err) {
-    //   console.log("Filename does not exist");
-    // } else {
-    //   console.log(obj.name);
-    // }
   });
 }
 
